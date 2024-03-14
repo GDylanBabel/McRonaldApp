cd31be0f778e6b83f0eee346a202020f1389279a
Guillermo Dylan Carvajal Aza <guillermo.carvajal@babelgroup.com>
2024-03-14 09:58:57 +0100

    Project Init

---
+++ src/main/java/com/helloworld/babel/McRonald/McRonaldApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.babel.McRonald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McRonaldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McRonaldApplication.class, args);
+	}
+
+}
